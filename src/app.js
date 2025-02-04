d2a5a52986ea290d5606d7f5efdc071e44d7001f
Nicolás Delfino <120816839+nicolasdelfino-123@users.noreply.github.com>
2025-02-04 23:10:08 +0000

    trabajo done

--- src/app.js
+++ src/app.js
@@ -7,11 +7,55 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let cartas = {
+    2: ["♦", "♥", "♠", "♣"],
+    3: ["♦", "♥", "♠", "♣"],
+    4: ["♦", "♥", "♠", "♣"],
+    5: ["♦", "♥", "♠", "♣"],
+    6: ["♦", "♥", "♠", "♣"],
+    7: ["♦", "♥", "♠", "♣"],
+    8: ["♦", "♥", "♠", "♣"],
+    9: ["♦", "♥", "♠", "♣"],
+    10: ["♦", "♥", "♠", "♣"],
+    J: ["♦", "♥", "♠", "♣"],
+    Q: ["♦", "♥", "♠", "♣"],
+    K: ["♦", "♥", "♠", "♣"],
+    A: ["♦", "♥", "♠", "♣"],
+  };
+
+  let claves = Object.keys(cartas); // me da todos las key en un array
+  let valores = Object.values(cartas); // me da todos los valores en un array
+
+  let randomNumero = Math.floor(Math.random() * claves.length); // me da el indice que es de 0 a 12 claves.length es de 1 a 13 - 1 = 12 porque math floor redondea hacia abajo
+  let claveSeleccionada = claves[randomNumero]; // arriba accedí al indice y acá le paso la clave + indice me da valor
+  let randomArray = Math.floor(Math.random() * valores.length);
+  let palos = cartas[claveSeleccionada];
+
+  let randomPalo = Math.floor(Math.random() * palos.length);
+  let paloSeleccionado = palos[randomPalo];
+
+
+  let palo = document.querySelectorAll(".paloFinal");
+  palo.forEach((palo) => {
+    palo.innerHTML = paloSeleccionado
+    if(paloSeleccionado === "♥" || paloSeleccionado === "♦"){
+      palo.style.color = "red"
+    } 
+    
+  })
+
+  let numero = document.getElementsByClassName("numero")
+  Array.from(numero).forEach((numero) => {
+    numero.innerHTML = claveSeleccionada
+  })
+
+  
 };
 
-/* let palo = [♦, ♥, ♠, ♣]
 
 
-if (diamantes | corazones)
-  randomPaloRojos += palo  */
+
+
+
+
